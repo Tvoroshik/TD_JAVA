d2324b470f3b169e336e94a6f0d032b590b8dfc6
Tvoroshik <Tvoroshik@gmail.com>
2022-12-09 16:09:13 +0500

    add sprites img

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,25 +1,43 @@
 package main;
+import java.io.IOException;
+import java.io.InputStream;
+import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Game extends JFrame {
 	
 	private GameScreen GameScreen;
 	
+	private BufferedImage img;
+	
 	public Game() {
 	
-		setSize(400,400);
+		importImg();
+		
+		setSize(640,640);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
+		
 		GameScreen = new GameScreen();
 		add(GameScreen);
 		
 	}
+private void importImg() {
 
+	InputStream is = getClass().getResourceAsStream("/res/spriteatlas.png");
+	
+	try {
+		img = ImageIO.read(is);
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+	
+}
 	public static void main(String[] args) {
-		System.out.println("start");
-		
+	
 		Game game=new Game();
 	}
 
--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -2,19 +2,36 @@ package main;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
 public class GameScreen extends JPanel{
 	
+	private Random random;
+	
 	public GameScreen() {
-		
+		random= new Random();
 	}
 	public void paintComponent(Graphics G) {
 		super.paintComponent(G);
-		
-		G.setColor(Color.red);
-		G.fillRect(50,50,100,100);
+	
+		for (int y=0; y<20; y++) {
+			for (int x=0; x<20; x++) {
+				G.setColor(getRndColor());
+				G.fillRect(x*32, y*32, 32, 32);	
+			}
+		}
 	}
 
+	private Color getRndColor() {
+		int r = random.nextInt(256);
+		int g = random.nextInt(256);
+		int b = random.nextInt(256);
+		
+		return new Color(r,g,b);
+		
+	}
+	
+	
 }
