aa998cc543561b7918bc19a1470fa1aab8d6adfc
Tvoroshik <Tvoroshik@gmail.com>
2022-12-24 23:42:40 +0500

    add lvl, edit 1 of this

--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -1,37 +1,34 @@
 package main;
 
-import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
-import java.util.Random;
-
 import javax.swing.JPanel;
 
-public class GameScreen extends JPanel{
-	
-	private Random random;
-	
-	public GameScreen() {
-		random= new Random();
-	}
-	public void paintComponent(Graphics G) {
-		super.paintComponent(G);
-	
-		for (int y=0; y<20; y++) {
-			for (int x=0; x<20; x++) {
-				G.setColor(getRndColor());
-				G.fillRect(x*32, y*32, 32, 32);	
-			}
-		}
+public class GameScreen extends JPanel {
+
+	private Game game;
+	private Dimension size;
+
+	public GameScreen(Game game) {
+		this.game = game;
+
+		setPanelSize();
 	}
 
-	private Color getRndColor() {
-		int r = random.nextInt(256);
-		int g = random.nextInt(256);
-		int b = random.nextInt(256);
-		
-		return new Color(r,g,b);
-		
+	private void setPanelSize() {
+		size = new Dimension(640, 640);
+
+		setMinimumSize(size);
+		setPreferredSize(size);
+		setMaximumSize(size);
+
 	}
-	
-	
+
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		game.getRender().render(g);
+
+	}
+
 }
---
+++ src/scenes/Playing.java
@@ -0,0 +1,33 @@
+package scenes;
+
+import java.awt.Graphics;
+
+import helpz.LevelBuild;
+import main.Game;
+import managers.TileManager;
+
+public class Playing extends GameScene implements SceneMethods {
+
+	private int[][] lvl;
+	private TileManager tileManager;
+
+	public Playing(Game game) {
+		super(game);
+
+		lvl = LevelBuild.getLevelData();
+		tileManager = new TileManager();
+
+	}
+
+	@Override
+	public void render(Graphics g) {
+
+		for (int y = 0; y < lvl.length; y++) {
+			for (int x = 0; x < lvl[y].length; x++) {
+				int id = lvl[y][x];
+				g.drawImage(tileManager.getSprite(id), x * 32, y * 32, null);
+			}
+		}
+
+	}
+}
