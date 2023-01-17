14a26d012d3fe59268400240bfd8323fe1929545
Tvoroshik <tvoroshik@gmail.com>
2023-01-17 12:36:46 +0500

    fix animations

--- src/scenes/Playing.java
+++ src/scenes/Playing.java
@@ -36,6 +36,7 @@ public class Playing extends GameScene implements SceneMethods {
 	}
 
 	public void update() {
+		updateTick();
 		enemyManager.update();
 	}
 
@@ -53,14 +54,15 @@ public class Playing extends GameScene implements SceneMethods {
 		for (int y = 0; y < lvl.length; y++) {
 			for (int x = 0; x < lvl[y].length; x++) {
 				int id = lvl[y][x];
-				g.drawImage(getSprite(id), x * 32, y * 32, null);
+				if (isAnimation(id)) {
+					g.drawImage(getSprite(id, animationIndex), x * 32, y * 32, null);
+				} else
+					g.drawImage(getSprite(id), x * 32, y * 32, null);
+			
 			}
 		}
 	}
 
-	private BufferedImage getSprite(int spriteID) {
-		return game.getTileManager().getSprite(spriteID);
-	}
 
 	@Override
 	public void mouseClicked(int x, int y) {
